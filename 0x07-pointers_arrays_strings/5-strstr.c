7655eb85732a36c2b18af3c1adda404e16e29689
Owura-Kwaku <eoa524@gmail.com>
2023-03-06 06:59:10 -0800

    pointers update

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,9 +10,13 @@ char *_strstr(char *haystack, char *needle)
 	while (*haystack)
 	{
 		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
+		{
 			return (haystack);
+		}
 		else
+		{
 			haystack++;
+		}
 	}
 	return (0);
 }
